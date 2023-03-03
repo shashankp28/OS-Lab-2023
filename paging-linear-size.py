1f54a82b709629bd7105d305603e4d82943c4bdd
Shashank P <shashankp2832@gmail.com>
2023-03-03 13:05:43 +0530

    python2 -> python3 corrections

--- paging-linear-size.py
+++ paging-linear-size.py
@@ -41,10 +41,10 @@ parser.add_option("-c", help="compute answers for me",
 
 (options, args) = parser.parse_args()
 
-print "ARG bits in virtual address", options.vsize
-print "ARG page size", options.pagesize
-print "ARG pte size", options.ptesize
-print ""
+print("ARG bits in virtual address", options.vsize)
+print("ARG page size", options.pagesize)
+print("ARG pte size", options.ptesize)
+print("")
 
 vsize    = int(options.vsize)
 ptesize  = int(convert(options.ptesize))
@@ -53,39 +53,39 @@ pagesize = int(convert(options.pagesize))
 # check: is page size a power of 2?
 offbits = int(math.log(float(pagesize))/math.log(2.0))
 if math.pow(2,offbits) != pagesize:
-    print 'Error in argument -p (pagesize): Must be a power of 2\n'
+    print('Error in argument -p (pagesize): Must be a power of 2\n')
     sys.exit(1)
 
 if options.solve == False:
-    print 'Compute how big a linear page table is with the characteristics you see above.\n'
-    print 'REMEMBER: There is one linear page table *per process*.'
-    print 'Thus, the more running processes, the more of these tables we will need.'
-    print 'However, for this problem, we are only concerned about the size of *one* table.\n'
+    print('Compute how big a linear page table is with the characteristics you see above.\n')
+    print('REMEMBER: There is one linear page table *per process*.')
+    print('Thus, the more running processes, the more of these tables we will need.')
+    print('However, for this problem, we are only concerned about the size of *one* table.\n')
 
 else:
-    print 'Recall that an address has two components: '
-    print '[ Virtual Page Number (VPN) | Offset ]'
-    print ''
-    print 'The number of bits in the virtual address:', vsize
-    print 'The page size:', pagesize, 'bytes'
-    print 'Thus, the number of bits needed in the offset:', offbits
+    print('Recall that an address has two components: ')
+    print('[ Virtual Page Number (VPN) | Offset ]')
+    print('')
+    print('The number of bits in the virtual address:', vsize)
+    print('The page size:', pagesize, 'bytes')
+    print('Thus, the number of bits needed in the offset:', offbits)
     vpnbits = vsize - offbits
-    print 'Which leaves this many bits for the VPN:', vpnbits
-    print 'Thus, a virtual address looks like this:\n'
+    print('Which leaves this many bits for the VPN:', vpnbits)
+    print('Thus, a virtual address looks like this:\n')
     for i in range(0,vpnbits):
-        print 'V',
-    print '|',
+        print('V',)
+    print('|',)
     for i in range(0,offbits):
-        print 'O',
-    print '\n\nwhere V is for a VPN bit and O is for an offset bit'
-    print 'To compute the size of the linear page table, we need to know:'
+        print('O',)
+    print('\n\nwhere V is for a VPN bit and O is for an offset bit')
+    print('To compute the size of the linear page table, we need to know:')
     entries = math.pow(2,vpnbits)
-    print '- The # of entries in the table, which is 2^(num of VPN bits):', entries
-    print '- The size of each page table entry, which is:', ptesize
-    print 'And then multiply them together. The final result:'
-    print ' ', int(entries * ptesize), 'bytes'
-    print '  in KB:', (entries * ptesize)/1024.0
-    print '  in MB:', (entries * ptesize)/1024.0/1024.0
+    print('- The # of entries in the table, which is 2^(num of VPN bits):', entries)
+    print('- The size of each page table entry, which is:', ptesize)
+    print('And then multiply them together. The final result:')
+    print(' ', int(entries * ptesize), 'bytes')
+    print('  in KB:', (entries * ptesize)/1024.0)
+    print('  in MB:', (entries * ptesize)/1024.0/1024.0)
     
     
     
--- relocation.py
+++ relocation.py
@@ -37,26 +37,26 @@ parser.add_option('-c', '--compute',   default=False, help='compute answers for 
 
 (options, args) = parser.parse_args()
 
-print ''
-print 'ARG seed', options.seed
-print 'ARG address space size', options.asize
-print 'ARG phys mem size', options.psize
-print ''
+print('')
+print('ARG seed', options.seed)
+print('ARG address space size', options.asize)
+print('ARG phys mem size', options.psize)
+print('')
 
 random.seed(options.seed)
 asize = convert(options.asize)
 psize = convert(options.psize)
 
 if psize <= 1:
-    print 'Error: must specify a non-zero physical memory size.'
+    print('Error: must specify a non-zero physical memory size.')
     exit(1)
 
 if asize == 0:
-    print 'Error: must specify a non-zero address-space size.'
+    print('Error: must specify a non-zero address-space size.')
     exit(1)
 
 if psize <= asize:
-    print 'Error: physical memory size must be GREATER than address space size (for this simulation)'
+    print('Error: physical memory size must be GREATER than address space size (for this simulation)')
     exit(1)
 
 #
@@ -76,41 +76,41 @@ if base == -1:
         if (base + limit) < psize:
             done = 1
 
-print 'Base-and-Bounds register information:'
-print ''
-print '  Base   : 0x%08x (decimal %d)' % (base, base)
-print '  Limit  : %d' % (limit)
-print ''
+print('Base-and-Bounds register information:')
+print('')
+print('  Base   : 0x%08x (decimal %d)' % (base, base))
+print('  Limit  : %d' % (limit))
+print('')
 
 if base + limit > psize:
-    print 'Error: address space does not fit into physical memory with those base/bounds values.'
-    print 'Base + Limit:', base + limit, '  Psize:', psize
+    print('Error: address space does not fit into physical memory with those base/bounds values.')
+    print('Base + Limit:', base + limit, '  Psize:', psize)
     exit(1)
 
 #
 # now, need to generate virtual address trace
 #
-print 'Virtual Address Trace'
+print('Virtual Address Trace')
 for i in range(0,options.num):
     vaddr = int(asize * random.random())
     if options.solve == False:
-        print '  VA %2d: 0x%08x (decimal: %4d) --> PA or segmentation violation?' % (i, vaddr, vaddr)
+        print('  VA %2d: 0x%08x (decimal: %4d) --> PA or segmentation violation?' % (i, vaddr, vaddr))
     else:
         paddr = 0
         if (vaddr >= limit):
-            print '  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION' % (i, vaddr, vaddr)
+            print('  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION' % (i, vaddr, vaddr))
         else:
             paddr = vaddr + base
-            print '  VA %2d: 0x%08x (decimal: %4d) --> VALID: 0x%08x (decimal: %4d)' % (i, vaddr, vaddr, paddr, paddr)
+            print('  VA %2d: 0x%08x (decimal: %4d) --> VALID: 0x%08x (decimal: %4d)' % (i, vaddr, vaddr, paddr, paddr))
 
 
-print ''
+print('')
 
 if options.solve == False:
-    print 'For each virtual address, either write down the physical address it translates to'
-    print 'OR write down that it is an out-of-bounds address (a segmentation violation). For'
-    print 'this problem, you should assume a simple virtual address space of a given size.'
-    print ''
+    print('For each virtual address, either write down the physical address it translates to')
+    print('OR write down that it is an out-of-bounds address (a segmentation violation). For')
+    print('this problem, you should assume a simple virtual address space of a given size.')
+    print('')
 
 
 
