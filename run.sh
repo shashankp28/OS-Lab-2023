e01819c6b990012e3f465957c0b955140fb42a9d
Shashank P <shashankp2832@gmail.com>
2023-03-26 22:32:09 +0530

    Added link open and read files with run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 cp open.c /usr/src/minix/servers/vfs/open.c
 cp link.c /usr/src/minix/servers/vfs/link.c
 cp read.c /usr/src/minix/servers/vfs/read.c
-cd /usr/src
-make build MKUPDATE=yes
+cd /usr/src && make build MKUPDATE=yes
